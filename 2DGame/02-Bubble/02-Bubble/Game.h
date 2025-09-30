0b5a78a4c06971453b9657e4195a5afa7f3e9fec
Marc <marc.rodriguez.camacho@estudiantat.upc.edu>
2025-09-30 15:49:46 +0200

    Update Game.h

--- 2DGame/02-Bubble/02-Bubble/Game.h
+++ 2DGame/02-Bubble/02-Bubble/Game.h
@@ -6,8 +6,8 @@
 #include "Scene.h"
 
 
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
+#define SCREEN_WIDTH 640*2
+#define SCREEN_HEIGHT 480*2
 
 
 // Game is a singleton (a class with a single instance) that represents our whole application
