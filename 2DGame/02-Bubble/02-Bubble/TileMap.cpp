3960e32fdbee960956a78e7defe6983168ec06af
Marc <marc.rodriguez.camacho@estudiantat.upc.edu>
2025-10-01 21:58:47 +0200

    Update TileMap.cpp

--- 2DGame/02-Bubble/02-Bubble/TileMap.cpp
+++ 2DGame/02-Bubble/02-Bubble/TileMap.cpp
@@ -192,11 +192,11 @@ bool TileMap::collisionMoveDown(const glm::ivec2 &pos, const glm::ivec2 &size, i
 	y = (pos.y + size.y - 1) / tileSize;
 	for(int x=x0; x<=x1; x++)
 	{
-		if(map[y*mapSize.x+x] != 0)
+		if(map[(y+1)*mapSize.x+x] != 0)
 		{
 			if(*posY - tileSize * y + size.y <= 4)
 			{
-				*posY = tileSize * y - size.y;
+				//*posY = tileSize * y - size.y;
 				return true;
 			}
 		}
